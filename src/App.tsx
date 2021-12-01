50cd655a5ac66a99af476a9e41dc832107a429b2
nikolay.tzonev <nhconev@gmail.com>
2021-12-01 14:40:30 +0200

    -added contact validation page; -added configuration;

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import configData from "./config.json";
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
+const modelReducer = (state: any, event: any) => {
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+}
+
 function App() {
+  const [model, setModel] = useReducer(modelReducer, {});
+  const [loading, setLoading] = useState(false);
+  const [fieldColor, setFieldColor] = useState('');
+  const [buttonState, setButtonState] = useState<{ front?: string, back?: string }>({});
+
+
+  const onFieldChange = (event: any) => {
+    setModel({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const updateBorderColor = (event: any) => {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    setFieldColor("#" + randomColor);
+  }
+
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+    setLoading(true);
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(model)
+    };
+    fetch(`${configData.apiBaseUrl}contact/validate`, requestOptions)
+      .then(res =>
+        setButtonState(res.ok ? { back: 'green', front: 'yellow' } : { back: 'red', front: 'blue' }))
+      .catch(console.log)
+      .finally(() => { setLoading(false); })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Contact Validation</h1>
+      <form onSubmit={onSubmit}>
+        <fieldset disabled={loading}>
+          <label htmlFor="firstName"><p>First name</p>
+            <input type="text" id="fname" name="firstName"
+              style={{ borderColor: fieldColor, borderWidth: 2 }}
+              value={model['firstName'] || ''} onChange={onFieldChange} onBlur={updateBorderColor} />
+          </label>
+          <label htmlFor="lastName"><p>Last name</p>
+            <input type="text" id="lname" name="lastName"
+              style={{ borderColor: fieldColor, borderWidth: 2 }}
+              value={model['lastName'] || ''} onChange={onFieldChange} onBlur={updateBorderColor} />
+          </label>
+          <label htmlFor="email"><p>Email</p>
+            <input type="text" id="email" name="email"
+              style={{ borderColor: fieldColor, borderWidth: 2 }}
+              value={model['email'] || ''} onChange={onFieldChange} onBlur={updateBorderColor} />
+          </label>
+        </fieldset>
+        <button type="submit" disabled={loading} style={{ backgroundColor: buttonState.back, color: buttonState.front }}>Submit</button>
+      </form>
     </div>
   );
 }
